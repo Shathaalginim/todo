692565d8578480fc99deef6b86aff7f88093ec7c
Moaz Amin <moazamin06@gmail.com>
2023-05-21 11:45:49 +0300

    Add, update, delete tasks

--- lib/Widgets/Task_list.dart
+++ lib/Widgets/Task_list.dart
@@ -4,25 +4,33 @@ import 'package:flutter/widgets.dart';
 
 class Task_list extends StatelessWidget {
   final bool isChecked;
+
   final String taskTitle;
+
   final void Function(bool?) checkboxChange;
+
+  final void Function() listTileDelete;
 
   Task_list(
       {required this.isChecked,
       required this.taskTitle,
-      required this.checkboxChange});
+      required this.checkboxChange,
+      required this.listTileDelete});
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text(
-          taskTitle,
-          style: TextStyle(
-              decoration: isChecked ? TextDecoration.lineThrough : null),
-        ),
-        trailing: Checkbox(
-          activeColor: Colors.teal[400],
-          value: isChecked,
-          onChanged: checkboxChange,
-        ));
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.teal[400],
+        value: isChecked,
+        onChanged: checkboxChange,
+      ),
+      onLongPress: listTileDelete,
+    );
   }
 }
